15b4abae07803730a0f075b75ea40c9114063bb9
Taka Okunishi <okunishinishi@gmail.com>
2015-08-13 08:24:19 +0900

    ADD README

--- render_examples.js
+++ render_examples.js
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env node
 
-coz render
+require('coz').render([
+    "01-minimum-demo/.*.bud",
+    "02-separated-template/.*.bud",
+    "04-from-programmatic-api/.*.bud",
+    "05-exec-bud-itself/.*.bud",
+    "06-customize-coz/.*.bud"
+], function (err) {
+    if (err) {
+        console.error(err);
+    }
+});
