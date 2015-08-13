213bec344aad1978f6ca60736846fdf5cf6efa4c
Taka Okunishi <okunishinishi@gmail.com>
2015-08-13 09:18:45 +0900

    Version incremented to 1.0.2

---
+++ index.js
@@ -0,0 +1,117 @@
+/**
+ * Examples for coz
+ * @module coz-examples
+ * @version 1.0.1
+ */
+
+"use strict";
+
+/** 01 Minimum Demo */
+exports['01-minimum-demo'] = {
+    'whoLikesWhatTxtBud': {
+        path: '01-minimum-demo/.who-likes-what.txt.bud',
+        type: 'javascript'
+    },
+    'whoLikesWhatTxt': {
+        path: '01-minimum-demo/who-likes-what.txt',
+        type: 'text'
+    }
+};
+
+
+/** 02 Separated Template */
+exports['02-separated-template'] = {
+    'whatColorsHtmlBud': {
+        path: '02-separated-template/.what-colors.html.bud',
+        type: 'javascript'
+    },
+    'whatColorsHtmlHbs': {
+        path: '02-separated-template/.what-colors.html.hbs',
+        type: 'html'
+    },
+    'whatColorsHtmlJson': {
+        path: '02-separated-template/.what-colors.html.json',
+        type: 'json'
+    },
+    'whatColorsHtml': {
+        path: '02-separated-template/what-colors.html',
+        type: 'html'
+    }
+};
+
+
+/** 04 From Programmatic Api */
+exports['04-from-programmatic-api'] = {
+    'travelByWhatTxtBud': {
+        path: '04-from-programmatic-api/.travel-by-what.txt.bud',
+        type: 'javascript'
+    },
+    'runRenderingJs': {
+        path: '04-from-programmatic-api/run_rendering.js',
+        type: 'javascript'
+    },
+    'travelByWhatTxt': {
+        path: '04-from-programmatic-api/travel-by-what.txt',
+        type: 'text'
+    }
+};
+
+
+/** 05 Exec Bud Itself */
+exports['05-exec-bud-itself'] = {
+    'execMeTxtBud': {
+        path: '05-exec-bud-itself/.exec-me.txt.bud',
+        type: 'javascript'
+    },
+    'execMeTxt': {
+        path: '05-exec-bud-itself/exec-me.txt',
+        type: 'text'
+    }
+};
+
+
+/** 06 Customize Coz */
+exports['06-customize-coz'] = {
+    'renderByCustomCliTxtBud': {
+        path: '06-customize-coz/.render-by-custom-cli.txt.bud',
+        type: 'javascript'
+    },
+    'renderWithCustomSetupTxtBud': {
+        path: '06-customize-coz/.render-with-custom-setup.txt.bud',
+        type: 'javascript'
+    },
+    'renderByCustomCliTxt': {
+        path: '06-customize-coz/render-by-custom-cli.txt',
+        type: 'text'
+    },
+    'renderByMyCustomEngine-01Txt': {
+        path: '06-customize-coz/render-by-my-custom-engine-01.txt',
+        type: 'text'
+    },
+    'renderByMyCustomTmpl-01Json': {
+        path: '06-customize-coz/render-by-my-custom-tmpl-01.json',
+        type: 'json'
+    },
+    'renderWithCustomEngineJs': {
+        path: '06-customize-coz/render-with-custom-engine.js',
+        type: 'javascript'
+    },
+    'renderWithCustomSetupTxt': {
+        path: '06-customize-coz/render-with-custom-setup.txt',
+        type: 'text'
+    },
+    'renderWithCustomTmplJs': {
+        path: '06-customize-coz/render-with-custom-tmpl.js',
+        type: 'javascript'
+    },
+    'useCustomConfigFromCliConfigJs': {
+        path: '06-customize-coz/use-custom-config-from-cli.config.js',
+        type: 'javascript'
+    },
+    'useCustomConfigFromCliSh': {
+        path: '06-customize-coz/use-custom-config-from-cli.sh',
+        type: 'text'
+    }
+};
+
+
